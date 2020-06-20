6e68a3e1e4fd4ca00ae84aa7d788827928ca34d4
Burstcol <contacto@burst.com.co>
2020-06-20 15:27:00 -0500

    First commit

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'Burst_P2p',
+	__DIR__
+);
---
+++ scripts/clearFolder.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /var/www/html/magento/app/app/code/Burst/P2p
+sudo rm -rf *
+sudo rm -rf .*
